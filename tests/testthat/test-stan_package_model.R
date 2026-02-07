398e1cc2b22893638298497a777cde54e71989f0
Tomotaka <momozaki.stat@gmail.com>
2026-02-07 13:12:02 +0900

    Add include_paths argument to stan_package_model()
    Add a formal include_paths parameter to stan_package_model() which gets
    passed to cmdstanr::cmdstan_model(). This follows the same pattern as
    the existing include_paths argument in stan_package_compile().
    
    Closes #33

--- tests/testthat/test-stan_package_model.R
+++ tests/testthat/test-stan_package_model.R
@@ -63,6 +63,40 @@ stan_test("stan_package_model(compile = TRUE)", {
   expect_true(is.data.frame(fit$summary()))
 })
 
+stan_test("stan_package_model(include_paths = ...)", {
+  skip_cmdstan()
+  path <- "example"
+  stan_package_create(path = path)
+  stan_package_configure(path = path)
+  temporary_library <- "library"
+  dir.create(temporary_library)
+  tmp <- utils::capture.output(
+    install.packages(
+      pkgs = path,
+      lib = temporary_library,
+      type = "source",
+      repos = NULL,
+      quiet = TRUE
+    )
+  )
+  stan_dir <- file.path(temporary_library, "example", "bin", "stan")
+  model <- stan_package_model(
+    name = "bernoulli",
+    package = "example",
+    library = temporary_library,
+    include_paths = stan_dir
+  )
+  tmp <- utils::capture.output(
+    fit <- model$sample(
+      data = list(N = 10, y = c(1, 0, 1, 0, 1, 0, 0, 0, 0, 0)),
+      refresh = 0,
+      iter_warmup = 2000,
+      iter_sampling = 4000
+    )
+  )
+  expect_true(is.data.frame(fit$summary()))
+})
+
 stan_test("stan_package_model() with deprecated inst", {
   skip_cmdstan()
   path <- "example"
